3698157bf6130dc233eb72387d22b7fb42f01df0
Gurjit Singh <102173730+Gurjit11@users.noreply.github.com>
2023-09-15 23:01:01 +0530

    Update views.py

--- autho/views.py
+++ autho/views.py
@@ -24,8 +24,6 @@ def home(request):
 
 
 def salesforce_login(request):
-    # Redirect the user to Salesforce for OAuth authentication
-    # Build the Salesforce OAuth authorization URL
     salesforce_authorize_url = (
         f"https://login.salesforce.com/services/oauth2/authorize"
         f"?response_type=code"
@@ -36,11 +34,9 @@ def salesforce_login(request):
 
 
 def salesforce_callback(request):
-    # Handle the callback from Salesforce after successful authentication
     authorization_code = request.GET.get("code")
 
     if authorization_code:
-        # Construct the request to get the access token
         token_url = "https://login.salesforce.com/services/oauth2/token"
         payload = {
             "code": authorization_code,
@@ -50,30 +46,23 @@ def salesforce_callback(request):
             "redirect_uri": settings.SALESFORCE_AUTH_REDIRECT_URI,
         }
 
-        # Make a POST request to Salesforce to exchange the authorization code for an access token
         response = requests.post(token_url, data=payload)
 
         if response.status_code == 200:
-            # Successfully obtained access token
             data = response.json()
             access_token = data.get("access_token")
             refresh_token = data.get("refresh_token")
             instance_url = data.get("instance_url")
 
-            # Get the authenticated user
             user = request.user
 
-            # Ensure the user is authenticated
             if user.is_authenticated:
-                # Create or retrieve SalesforceOAuthToken for the user
                 profile, created = SalesforceOAuthToken.objects.get_or_create(user=user)
                 profile.access_token = access_token
                 profile.refresh_token = refresh_token
                 profile.instance_url = instance_url
                 profile.save()
 
-            # Redirect to a success page or perform further actions
             return HttpResponse("welcome to Mavlon")
 
-    # Handle the case where the OAuth process failed
     return HttpResponse("OAuth process failed. Please try again.")
